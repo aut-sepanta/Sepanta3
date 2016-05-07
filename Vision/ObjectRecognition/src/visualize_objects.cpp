a90f7587c440d9dc8cb6dfade48384cda8b26373
Mohammad Hossein Heydari <mdh.heydari@gmail.com>
2016-05-07 13:22:13 +0430

    demo 2 finished , amcl start , manager done

--- Vision/ObjectRecognition/src/visualize_objects.cpp
+++ Vision/ObjectRecognition/src/visualize_objects.cpp
@@ -4,7 +4,7 @@
 #include <message_filters/sync_policies/approximate_time.h>
 
 #include <sensor_msgs/Image.h>
-#include <sepanta_msgs/Objects.h>
+#include <sepanta_msgs/Object.h>
 #include <image_transport/image_transport.h>
 
 #include <cv_bridge/cv_bridge.h>
@@ -12,19 +12,17 @@
 #include <boost/thread/mutex.hpp>
 
 boost::mutex mutex;
-sepanta_msgs::ObjectsConstPtr objects;
+sepanta_msgs::ObjectConstPtr object;
 bool first_run = true;
 
-void objectsCallback(const sepanta_msgs::ObjectsConstPtr& objs) {
+void objectCallback(const sepanta_msgs::ObjectConstPtr& obj) {
     mutex.lock();
-    objects = objs;
+    object = obj;
     mutex.unlock();
     first_run=false;
 }
 
 void rgbImageCallback(const sensor_msgs::ImageConstPtr& input_image) {
-   
-
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(input_image, sensor_msgs::image_encodings::BGR8);
 
@@ -32,19 +30,18 @@ void rgbImageCallback(const sensor_msgs::ImageConstPtr& input_image) {
     cv::Scalar color;
     if (!first_run) {
         mutex.lock();
-        for(size_t i=0; i<objects->objects.size(); i++) {
-            position = objects->objects[i].center_2d;
+        position = object->center_2d;
 
-            if (objects->objects[i].status) {
-                color = CV_RGB(255, 0, 0);
-            } else {
-                color = CV_RGB(0, 255, 0);
-            }
-
-            ROS_INFO_STREAM(cv::Point(int(position.x), int(position.y)));
-            cv::circle(cv_ptr->image, cv::Point(int(position.x), int(position.y)), 3, color);
-            cv::putText(cv_ptr->image, objects->objects[i].label, cv::Point(int(position.x+10), int(position.y+10)), cv::FONT_HERSHEY_PLAIN, 3, color);
+        if (object->status) {
+           color = CV_RGB(255, 0, 0);
+        } else {
+            color = CV_RGB(0, 255, 0);
         }
+
+        ROS_INFO_STREAM(cv::Point(int(position.x), int(position.y)));
+        cv::circle(cv_ptr->image, cv::Point(int(position.x), int(position.y)), 3, color);
+        cv::putText(cv_ptr->image, object->label, cv::Point(int(position.x+10), int(position.y+10)), cv::FONT_HERSHEY_PLAIN, 3, color);
+        cv::rectangle(cv_ptr->image, cv::Point(int(position.x)-40, int(position.y)-40), cv::Point(int(position.x)+30, int(position.y)+90), color, 3);
         mutex.unlock();
     }
     cv::imshow("Objects Visualizer", cv_ptr->image);
@@ -57,7 +54,7 @@ int main(int argc, char** argv)
     ros::NodeHandle node_handle;
 
     ros::Subscriber rgb_subscriber = node_handle.subscribe("/camera/rgb/image_color", 1, rgbImageCallback);
-    ros::Subscriber objs_subscriber = node_handle.subscribe("/object_recognition/objects", 1, objectsCallback);
+    ros::Subscriber objs_subscriber = node_handle.subscribe("/behavior/current_object", 1, objectCallback);
 
     ros::spin();
 
